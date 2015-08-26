35baff599209c2f956b4da2a42c2e0c8891ce4bf
Finn Grimwood <finngrimwood@gmail.com>
2015-08-26 23:16:48 +0100

    whitespace

--- ram.cpp
+++ ram.cpp
@@ -5,21 +5,21 @@
 
 Chip8Ram::Chip8Ram()
 {
-    /* Init the ram by zeroing out all the memory */
-    LOG(INFO) << "Start up, clearing memory";
-    for(int8_t byte : ram) {
-        byte = 0;
-    }
+	/* Init the ram by zeroing out all the memory */
+	LOG(INFO) << "Start up, clearing memory";
+	for(int8_t byte : ram) {
+		byte = 0;
+	}
 }
 
 int8_t& Chip8Ram::operator[](std::size_t i)
 {
-    return ram[i];
+	return ram[i];
 }
 
 const int8_t& Chip8Ram::operator[](std::size_t i) const
 {
-    return ram[i];
+	return ram[i];
 }
 
 uint16_t Chip8Ram::read_instruction(uint16_t addr)
